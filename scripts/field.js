088be97c48dbad59de1dd2e81105ce56a57219b0
Ben Bowles <55298859+HoleInOneGolfer@users.noreply.github.com>
2022-01-04 17:40:33 -0600

    fieldUpdate

--- scripts/field.js
+++ scripts/field.js
@@ -7,12 +7,12 @@ const Field = {
         bg: 'rgb(50, 150, 50)',
         lnCol: 'rgb(255, 255, 255)',
         ydln10w: 4,
-        ydln5w: 2,
+        ydln5w: 1,
         hlnw: 3,
         fontSize: 5
     },
 
-    draw: function(){
+    draw: function () {
         background(this.style.bg); // Fill background
 
         //Draw yard lines and text
@@ -20,41 +20,40 @@ const Field = {
         fill(255, 255, 255);
         textSize(this.style.fontSize * scale);
 
-        for (let i = -45; i < 50; i+=5) {
-            switch (i % 10) {
+        for (let i = -45; i < 50; i += 5) {
+            switch (Math.abs(i % 10)) {
+                case 0:
+                    strokeWeight(this.style.ydln10w);
+                    if (i < 0) {
+                        text(Math.abs(i + 50), (i / 10 * this.width / 10 - this.style.fontSize / 2) * scale, (this.height - this.style.fontSize) * scale / 2);
+                        text(Math.abs(i + 50), (i / 10 * this.width / 10 - this.style.fontSize / 2) * scale, (this.style.fontSize * 2.5 - this.height) * scale / 2);
+                    }
+                    else {
+                        text(50 - i, (i / 10 * this.width / 10 - this.style.fontSize / 2) * scale, (this.height - this.style.fontSize) * scale / 2);
+                        text(50 - i, (i / 10 * this.width / 10 - this.style.fontSize / 2) * scale, (this.style.fontSize * 2.5 - this.height) * scale / 2);
+                    }
+                    break;
                 case 5:
                     strokeWeight(this.style.ydln5w);
 
                     break;
-            
-                case 0:
-                    strokeWeight(this.style.ydln10w);
-                    if ( i < 0){
-                        text(i, (i/10*this.width/10 - this.style.fontSize / 2) * scale, 0);
-                    }
-                    else
-                    { 
-                        text(i, (i/10*this.width/10 + this.style.fontSize / 2) * scale, 0);
-                    }
 
-
-                    break;
             }
-            line ( i/10*this.width/10 * scale, -this.height / 2 * scale,  i/10*this.width/10 * scale, this.height / 2 * scale);
+            line(i / 10 * this.width / 10 * scale, -this.height / 2 * scale, i / 10 * this.width / 10 * scale, this.height / 2 * scale);
         }
 
         //Draw Hashes
         strokeWeight(this.style.hlnw);
         drawingContext.setLineDash([10]);
 
-        line(-this.width/2 * scale, this.hash_Distance * scale / 2, this.width/2 * scale, this.hash_Distance * scale / 2);
-        line(-this.width/2 * scale, this.hash_Distance * scale / -2, this.width/2 * scale, this.hash_Distance * scale / -2);
+        line(-this.width / 2 * scale, this.hash_Distance * scale / 2, this.width / 2 * scale, this.hash_Distance * scale / 2);
+        line(-this.width / 2 * scale, this.hash_Distance * scale / -2, this.width / 2 * scale, this.hash_Distance * scale / -2);
 
         //reset
         drawingContext.setLineDash([0]);
         stroke(0);
         fill(0);
         strokeWeight(1);
-        
+
     }
 }
