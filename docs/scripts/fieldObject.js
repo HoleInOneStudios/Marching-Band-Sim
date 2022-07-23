d1995ac9d4489368a1a1eb51a6a49e5d500e5bc8
Ben Bowles <benjamin.d.bowles@gmail.com>
2022-07-23 15:54:36 -0500

    obj update loop

--- docs/scripts/fieldObject.js
+++ docs/scripts/fieldObject.js
@@ -4,17 +4,34 @@ class FieldObject {
     }
 
     async show() {
-        console.log("Shown");
+
     }
 
-    async update() {
-        console.log("Updated");
+    async update(set, count) {
+
     }
 }
 
 class Objects {
-    constructor () {
+    constructor (maxCount, interval) {
         this.List = [];
+        this.selected = this.List[0];
+
+        this.set = 0;
+        this.maxSet = 0;
+        this.count = 0;
+        this.maxCount = maxCount || 4;
+
+        this.time = 0;
+        this.interval = interval || 10;
+    }
+
+    add(obj) {
+        return this.List.push(obj);
+    }
+
+    remove(obj) {
+        return this.List.splice(this.List.indexOf(obj), 1);
     }
 
     async show() {
@@ -24,8 +41,23 @@ class Objects {
     }
 
     async update() {
+        this.time++;
+        if (this.time >= this.interval) {
+            this.count++;
+            this.time = 0;
+            //console.log("frame");
+            if (this.count >= this.maxCount) {
+                this.currentSet++;
+                this.count = 0;
+                //console.log("count");
+                if (this.currentSet >= this.maxSet) {
+                    this.currentSet = 0;
+                    //console.log("set");
+                }
+            }
+        }
         this.List.forEach(element => {
-            element.update();
+            element.update(this.set, this.count);
         });
     }
 }
--- docs/scripts/mbs.js
+++ docs/scripts/mbs.js
@@ -1,11 +1,15 @@
 import { Field } from "./Field.js";
-import { FieldObject } from "./FieldObject.js";
+import { FieldObject, Objects } from "./FieldObject.js";
 
 let field;
 let fieldObj;
+let objs;
 
 document.body.onload = async () => {
     field = new Field("canvas", 300 / 3, 160 / 3, 60 / 3, 10, "white", "green", "fieldControls", "bgColor", "lnColor", "lnWidth", "mouseX", "mouseY");
+
+    objs = new Objects();
+    objs.add(new FieldObject());
 
     addEventListener('resize', () => {
         field.resize();
@@ -14,6 +18,9 @@ document.body.onload = async () => {
     setInterval(() => {
         field.draw();
         field.update();
+
+        objs.update();
+        objs.show();
 
     }, 1000 / 60);
 }
