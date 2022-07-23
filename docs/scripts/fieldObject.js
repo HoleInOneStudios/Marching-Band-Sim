588b9ae667280f495b555d3ca4197583c536b868
Ben Bowles <benjamin.d.bowles@gmail.com>
2022-07-23 15:35:38 -0500

    Ui and field object starts

--- docs/scripts/fieldObject.js
+++ docs/scripts/fieldObject.js
@@ -1,9 +1,33 @@
 class FieldObject {
+    constructor () {
+        
+    }
 
+    async show() {
+        console.log("Shown");
+    }
+
+    async update() {
+        console.log("Updated");
+    }
 }
 
 class Objects {
-    
+    constructor () {
+        this.List = [];
+    }
+
+    async show() {
+        this.List.forEach(element => {
+            element.show();
+        });
+    }
+
+    async update() {
+        this.List.forEach(element => {
+            element.update();
+        });
+    }
 }
 
 export { FieldObject, Objects };
