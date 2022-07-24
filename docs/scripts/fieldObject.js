d81e764d53e0e013b979a4b0006e13c7deaf19aa
Ben Bowles <benjamin.d.bowles@gmail.com>
2022-07-24 15:40:07 -0500

    prev, current, next set, count display and upadte

--- docs/scripts/fieldObject.js
+++ docs/scripts/fieldObject.js
@@ -2,25 +2,32 @@ import { lerp, lerp2 } from "./Math.js";
 
 class FieldObject {
     constructor () {
-        this.sets = [{ x: 0, y: 0 }, { x: 0, y: 0 }];
+        this.sets = [{ x: 10, y: 10 }, { x: 100, y: 100 }, { x: 10, y: 10 }];
         this.pos = { x: 0, y: 0 };
     }
 
-    async show(ctx) {
-        
+    async show(field) {
     }
 
-    async update(prevSet, set, count, maxCount) {
-        this.pos = lerp2(this.sets[prevSet], this.sets[set], count/maxCount);
+    async update(objects) {
+        
     }
 }
 
 class Objects {
-    constructor (maxCount, interval) {
+    constructor (maxCount, interval, nextSetDis, currentSetDis, previousSetDis, countDis) {
         this.List = [];
         this.selected = this.List[0];
 
-        this.set = 0;
+        this.nextSet = 0;
+        this.currentSet = 0;
+        this.previousSet = 0;
+
+        this.nextSetDis = document.getElementById(nextSetDis);
+        this.currentSetDis = document.getElementById(currentSetDis);
+        this.previousSetDis = document.getElementById(previousSetDis);
+        this.countDis = document.getElementById(countDis);
+
         this.maxSet = 0;
         this.count = 0;
         this.maxCount = maxCount || 4;
@@ -37,31 +44,41 @@ class Objects {
         return this.List.splice(this.List.indexOf(obj), 1);
     }
 
-    async show() {
+    async show(field) {
         this.List.forEach(element => {
-            element.show();
+            element.show(field);
         });
     }
 
     async update() {
         this.time++;
-        if (this.time > this.interval) {
+        if (this.time >= this.interval) {
             this.count++;
             this.time = 0;
             //console.log("frame");
-            if (this.count > this.maxCount) {
+            if (this.count >= this.maxCount) {
                 this.currentSet++;
+                this.nextSet = (this.currentSet + 1) % this.maxSet;
+                this.previousSet = (this.currentSet - 1) % this.maxSet;
                 this.count = 0;
                 //console.log("count");
-                if (this.currentSet > this.maxSet) {
+                if (this.currentSet >= this.maxSet) {
                     this.currentSet = 0;
                     //console.log("set");
                 }
             }
         }
         this.List.forEach(element => {
-            element.update(this.set ,this.set+1, this.count, this.maxCount);
+            if (element.sets.length - 1 > this.maxSet) {
+                this.maxSet = element.sets.length;
+            }
+            element.update(this);
         });
+
+        this.nextSetDis.innerText = this.nextSet;
+        this.currentSetDis.innerText = this.currentSet;
+        this.previousSetDis.innerText = this.previousSet;
+        this.countDis.innerText = this.count;
     }
 }
 
--- docs/scripts/mbs.js
+++ docs/scripts/mbs.js
@@ -8,7 +8,7 @@ let objs;
 document.body.onload = async () => {
     field = new Field("canvas", 300 / 3, 160 / 3, 60 / 3, 10, "white", "green", "fieldControls", "bgColor", "lnColor", "lnWidth", "mouseX", "mouseY");
 
-    objs = new Objects();
+    objs = new Objects(4, 20, "nextSet", "currentSet", "prevSet", "count");
     objs.add(new FieldObject());
 
     addEventListener('resize', () => {
@@ -20,7 +20,7 @@ document.body.onload = async () => {
         field.update();
 
         objs.update();
-        objs.show();
+        objs.show(field);
 
     }, 1000 / 60);
 }
